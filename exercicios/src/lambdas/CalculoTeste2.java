6677c068f5972811c21919818a99b9ab97ebc359
MatheusSousa98 <matheusousams.98@gmail.com>
2023-01-05 16:40:22 -0300

    Aula 174 - (O commit anterior foi da aula 173)

--- exercicios/src/lambdas/CalculoTeste2.java
+++ exercicios/src/lambdas/CalculoTeste2.java
@@ -5,9 +5,12 @@ public class CalculoTeste2 {
 	public static void main(String[] args) {
 		
 		Calculo calc = (x, y) -> {
-			return x - y;
+			return x + y;
 		};
-		
 		System.out.println(calc.executar(1, 2));
+		
+		calc = (x, y) -> x * y;
+		System.out.println(calc.executar(1, 2));
+		
 	}
 }
