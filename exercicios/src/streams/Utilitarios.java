7d819e98612e797f1a90dd001d130917ea7ab235
MatheusSousa98 <matheusousams.98@gmail.com>
2023-01-10 15:35:26 -0300

    Aula 197

---
+++ exercicios/src/streams/Utilitarios.java
@@ -0,0 +1,21 @@
+package streams;
+
+import java.util.function.UnaryOperator;
+
+public class Utilitarios {
+
+	private Utilitarios() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public final static UnaryOperator<String> maiuscula =
+			n -> n.toUpperCase();
+			
+	public final static UnaryOperator<String> primeiraLetra =
+			n -> n.charAt(0) + "";
+					
+	public static String grito(String n) {
+		return n + "!!! ";
+	}
+	
+}
