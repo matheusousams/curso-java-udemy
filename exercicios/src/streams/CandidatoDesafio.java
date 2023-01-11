c0b2529a1e1b389526374943cc72f59472247eb4
MatheusSousa98 <matheusousams.98@gmail.com>
2023-01-11 13:43:23 -0300

    Aula 202 - Desafio Filter

---
+++ exercicios/src/streams/CandidatoDesafio.java
@@ -0,0 +1,15 @@
+package streams;
+
+public class CandidatoDesafio {
+
+	final String nome;
+	final int idade;
+	final boolean pagouMatricula;
+
+	public CandidatoDesafio(String nome, int idade, boolean pagouMatricula) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.pagouMatricula = pagouMatricula;
+	}
+}
---
+++ exercicios/src/streams/DesafioFilter.java
@@ -0,0 +1,31 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class DesafioFilter {
+
+	public static void main(String[] args) {
+		
+		CandidatoDesafio candidato1 = new CandidatoDesafio("João", 18, true);
+		CandidatoDesafio candidato2 = new CandidatoDesafio("Maria", 19, false);
+		CandidatoDesafio candidato3 = new CandidatoDesafio("Ana", 17, true);
+		
+		List<CandidatoDesafio> candidatos = Arrays.asList(candidato1, candidato2, candidato3);
+		
+		Predicate<CandidatoDesafio> temIdadeMinima = d -> d.idade >= 18;
+		Predicate<CandidatoDesafio> pagouMatricula = d -> d.pagouMatricula == true;
+		
+		Function<CandidatoDesafio, String> podeIniciarAulas = d -> "Bem-vindo as aulas, " + d.nome + ".";
+		
+		candidatos.stream()
+			.filter(temIdadeMinima)
+			.filter(pagouMatricula)
+			.map(podeIniciarAulas)
+			.forEach(System.out::println);
+		
+	}	
+	
+}
