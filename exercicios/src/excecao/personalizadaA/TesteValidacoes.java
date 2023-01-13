c73ac311e3ee0bddd2185a0e612d8af92d6ee389
MatheusSousa98 <matheusousams.98@gmail.com>
2023-01-13 12:49:56 -0300

    Aula 220

---
+++ exercicios/src/excecao/personalizadaA/TesteValidacoes.java
@@ -0,0 +1,21 @@
+package excecao.personalizadaA;
+
+import excecao.Aluno;
+
+public class TesteValidacoes {
+
+	public static void main(String[] args) {
+		
+		try {
+			Aluno aluno = new Aluno("Kaio", 5);
+			Validar.aluno(aluno);
+			Validar.aluno(null);
+		} catch (StringVaziaException | NumeroForaDoIntervaloException e) {
+			System.out.println(e.getMessage());
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("Fim!");
+	}
+}
