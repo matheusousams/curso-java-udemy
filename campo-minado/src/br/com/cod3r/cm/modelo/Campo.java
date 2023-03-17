c8509e4cd48e81c1ed49a38c3f78f39787987c64
MatheusSousa98 <matheusousams.98@gmail.com>
2023-03-17 14:49:39 -0300

    Aula 230 - Classe Campo: Atributos

--- campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -1,5 +1,25 @@
 package br.com.cod3r.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 
+	private final int row;
+	private final int column;
+	
+	private boolean aberto = false;
+	private boolean marcado = false;
+	private boolean minado = false;
+	
+	private List<Campo> vizinhos = new ArrayList<>();
+	
+	Campo(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		
+	}
 }
