d54999033f9aabce07a112d6233c71f93f80a8e4
matheusousams <matheusousams.98@gmail.com>
2024-04-07 23:41:08 -0300

    Aula 241 - Classe Tabuleiro Console

--- campo-minado/src/br/com/cod3r/cm/Aplicacao.java
+++ campo-minado/src/br/com/cod3r/cm/Aplicacao.java
@@ -1,17 +1,14 @@
 package br.com.cod3r.cm;
 
 import br.com.cod3r.cm.modelo.Tabuleiro;
+import br.com.cod3r.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
-		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternaMarcacao(4, 4);
-		tabuleiro.alternaMarcacao(4, 5);
+		new TabuleiroConsole(tabuleiro);
 
-		System.out.println(tabuleiro);
 	}
 }
