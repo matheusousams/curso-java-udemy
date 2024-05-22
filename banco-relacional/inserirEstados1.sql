fec4e5c3560690c74908e2ed81c8d1fdc7409375
matheusousams <matheusousams.98@gmail.com>
2024-05-21 23:23:50 -0300

    Aula 293 - Inserir Estados

---
+++ banco-relacional/inserirEstados1.sql
@@ -0,0 +1,10 @@
+insert into estados (nome, sigla, regiao, populacao)
+values ('Ceara', 'CE', 'Nordeste', 8.84);
+
+insert into estados
+    (nome, sigla, regiao, populacao)
+values
+    ('São Paulo', 'SP', 'Sudeste', 44.04),
+    ('Rio Grande do Sul', 'RS', 'Sul', 11.29),
+    ('Roraima', 'RR', 'Norte', 0.5),
+    ('Brasília', 'DF', 'Centro-Oeste', 2.82);
