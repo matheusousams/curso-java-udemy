f2e57b31bffb3561e109bd6abbdf1e10bf0ecd32
matheusousams <matheusousams.98@gmail.com>
2024-05-27 18:33:45 -0300

    Aula 257 - Named Native Query

---
+++ exercicios-jpa/src/main/java/teste/consulta/ObterMediaDosFilmes.java
@@ -0,0 +1,17 @@
+package teste.consulta;
+
+import infra.DAO;
+import modelo.consulta.NotaFilme;
+
+public class ObterMediaDosFilmes {
+
+    public static void main(String[] args) {
+
+        DAO<NotaFilme> dao = new DAO<>(NotaFilme.class);
+        NotaFilme nota = dao.consultarUm("mediaGeralDosFilmes");
+
+        System.out.println(nota.getMedia());
+
+        dao.fechar();
+    }
+}
