7477fb7edfb47d832e00f84020fc3d6ad275724d
matheusousams <matheusousams.98@gmail.com>
2024-05-27 18:45:55 -0300

    Aula 358 - Embeddable

---
+++ exercicios-jpa/src/main/java/modelo/composicao/Fornecedor.java
@@ -0,0 +1,49 @@
+package modelo.composicao;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "fornecedores")
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Endereco endereco;
+
+    public Fornecedor() {
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+}
