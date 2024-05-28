b092dbd1c2fe795cfe2eee4c46a6ec15ee1155d3
matheusousams <matheusousams.98@gmail.com>
2024-05-28 17:50:02 -0300

    Aula 425 - Passando Parâmetros para Web Service #01

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,10 @@ public class ClienteController {
     @GetMapping(path = "/qualquer")
     public ClienteModel obterClienteModel() {
         return new ClienteModel(30, "Valéria", "123.456.789-10");
+    }
+
+    @GetMapping("/{id}")
+    public ClienteModel obterClientePorId(@PathVariable int id) {
+        return new ClienteModel(id, "Joana", "111.222.444-33");
     }
 }
