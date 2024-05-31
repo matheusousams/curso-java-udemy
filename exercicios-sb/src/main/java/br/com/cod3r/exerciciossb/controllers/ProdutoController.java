9f5707b1897aa5b9e8d80c0ef5e8a4059466a9e1
matheusousams <matheusousams.98@gmail.com>
2024-05-31 19:13:03 -0300

    Aula 439 - Simplificando Inserir Produtos

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -3,7 +3,6 @@ package br.com.cod3r.exerciciossb.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +17,7 @@ public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
     @PostMapping
-    public @ResponseBody ProdutoModel novoProduto(@RequestParam String nome, @RequestParam double preco,
-            @RequestParam int desconto) {
-        ProdutoModel produto = new ProdutoModel(nome, preco, desconto);
+    public @ResponseBody ProdutoModel novoProduto(ProdutoModel produto) {
         produtoRepository.save(produto);
         return produto;
     }
