9bb072e57c972f0ca96bc075124f98f33be230f0
matheusousams <matheusousams.98@gmail.com>
2024-06-09 11:32:53 -0300

    Ajustes

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -3,13 +3,13 @@ package br.com.cod3r.exerciciossb.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,29 +44,25 @@ public class ProdutoController {
         return produtoRepository.searchByNameLike(parteNome);
     }
 
-    /*
-     * @GetMapping(path = "/pagina/{numeroPagina}/qtdePagina")
-     * public Iterable<ProdutoModel> obterProdutosPorPagina(@PathVariable int
-     * numeroPagina, @PathVariable int qtdePagina) {
-     * if (qtdePagina >= 5)
-     * qtdePagina = 5;
-     * Pageable page = PageRequest.of(numeroPagina, qtdePagina);
-     * return produtoRepository.findAll(page);
-     * }
-     */
+    @GetMapping(path = "/pagina/{numeroPagina}/qtdePagina")
+    public Iterable<ProdutoModel> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina) {
+        if (qtdePagina >= 5)
+            qtdePagina = 5;
+        Pageable page = PageRequest.of(numeroPagina, qtdePagina);
+        
+        return produtoRepository.findAll(page);
+    }
 
     @GetMapping(path = "/{id}")
     public Optional<ProdutoModel> obterProdutosPorId(@PathVariable int id) {
         return produtoRepository.findById(id);
     }
 
-    /*
-     * @PutMapping
-     * public ProdutoModel alteraProduto(@Valid ProdutoModel produto) {
-     * produtoRepository.save(produto);
-     * return produto;
-     * }
-     */
+    @PutMapping
+    public ProdutoModel alteraProduto(@Valid ProdutoModel produto) {
+        produtoRepository.save(produto);
+        return produto;
+    }
 
     @DeleteMapping(path = "/{id}")
     public void excluirProduto(@PathVariable int id) {
